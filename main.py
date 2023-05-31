fc0bc407360c274cd7fff9bef4961adc2b1a54bb
Embun Duta Laksmana <82716426+embundutalaksmana@users.noreply.github.com>
2023-05-31 19:08:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import io
 import os
 import uvicorn
 
+
 app = FastAPI()
 
-model = tf.keras.models.load_model("model.h5")
+model_path = os.path.join(os.path.dirname(__file__), 'model1.h5')
+model = tf.keras.models.load_model(model_path)
 
-classification_result = None  # Definisikan variabel classification_result
+classification_result = None
+labels = ["HDPE", "LDPE", "OTHERS", "PET", "PP", "PS", "PVC"]
+
 
 def classify_image(image):
     # Preprocess the image if needed
@@ -19,18 +23,23 @@ def classify_image(image):
     return predictions
 
 
+def get_highest_label(predictions):
+    highest_index = tf.argmax(predictions, axis=1)[0]
+    return labels[highest_index]
+
+
 @app.post("/upload-photo")
 async def upload_photo(file: UploadFile = File(...)):
-    contents = await file.read()
-    image = tf.image.decode_image(contents, channels=3)
-    image = tf.image.resize(image, [224, 224])
+     contents = await file.read()
+     image = tf.image.decode_image(contents, channels=3)
+     image = tf.image.resize(image, [224, 224])
 
-    predictions = classify_image(tf.expand_dims(image, axis=0))
+     predictions = classify_image(tf.expand_dims(image, axis=0))
 
-    global classification_result
-    classification_result = predictions.tolist()  # Simpan hasil klasifikasi ke dalam classification_result
+     global classification_result
+     classification_result = predictions.tolist()  # Simpan hasil klasifikasi ke dalam classification_result.
 
-    return JSONResponse(content={"predictions": predictions.tolist()})
+     return JSONResponse(content={"predictions": predictions.tolist()})
 
 
 @app.get("/classification-result")
@@ -38,7 +47,7 @@ async def get_classification_result():
     global classification_result
 
     if classification_result:
-        result = classification_result
+        result = get_highest_label(classification_result)
         classification_result = None
         return JSONResponse(content={"result": result})
     else:
