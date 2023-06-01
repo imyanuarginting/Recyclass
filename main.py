c81e264669355c5541f48aa29c5bcf8c1f349f98
embundutalaksmana <embun20ti@mahasiswa.pcr.ac.id>
2023-06-02 01:03:20 +0700

    CC Perubahan 13 | Penambahan Endpoint Lokasii

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import io
 import os
 import uvicorn
-
+import json
 
 app = FastAPI()
 
@@ -27,7 +27,7 @@ def get_highest_label(predictions):
     highest_index = tf.argmax(predictions, axis=1)[0]
     return labels[highest_index]
 
-
+#endpoints for posting image to server
 @app.post("/upload-photo")
 async def upload_photo(file: UploadFile = File(...)):
      contents = await file.read()
@@ -41,8 +41,8 @@ async def upload_photo(file: UploadFile = File(...)):
 
      return JSONResponse(content={"predictions": predictions.tolist()})
 
-
-@app.get("/classification-result")
+# endpoint for getting data from server 
+@app.get("/classification-result") 
 async def get_classification_result():
     global classification_result
 
@@ -53,6 +53,26 @@ async def get_classification_result():
     else:
         return JSONResponse(content={"result": None})
 
+
+@app.get("/lokasi/{city}")
+async def get_city_data(city: str):
+    file_path = "tempat.json"  # Ubah sesuai dengan path file JSON Anda
+    with open(file_path) as file:
+        data = json.load(file)
+    
+    if city in data:
+        result = data[city]
+        return JSONResponse(content=result)
+    else:
+        return JSONResponse(content={"error": "Lokasi Bank Sampah tidak ditemukan"})
+
+@app.get("/artikel")
+async def get_article():
+    file_path = "artikel.json"  # Ubah sesuai dengan path file JSON artikel Anda
+    with open(file_path) as file:
+        data = json.load(file)
+    
+    return JSONResponse(content=data)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
