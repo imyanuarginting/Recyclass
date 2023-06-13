24d7ac3e248a6269b88a3dab6e2b2680fe6e331d
kartikaameliarahmawati <kartikaameliarahmawati141@gmail.com>
2023-06-13 10:42:02 +0700

    Feature: Insert image from gallery, change launcher icon, adding scrollbar

--- app/src/main/java/com/example/recyclass/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/example/recyclass/ui/camera/CameraActivity.kt
@@ -3,6 +3,7 @@ package com.example.recyclass.ui.camera
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -11,6 +12,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
+import com.example.recyclass.R
 import com.example.recyclass.SettingApplication
 import com.example.recyclass.data.Indicator
 import com.example.recyclass.databinding.ActivityCameraBinding
@@ -19,17 +21,22 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
+    private lateinit var viewModel: CameraViewModel
+    private lateinit var currentImagePath: String
+    private lateinit var getImage: File
+    private var plasticType = ""
     private val timeStamp = SimpleDateFormat(
         TIME_STAMP_FORMAT,
         Locale.US
     ).format(System.currentTimeMillis())
-    private lateinit var currentImagePath: String
-    private var getImage: File? = null
 
     private val launcherCamera = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
@@ -43,9 +50,18 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
-    private var plasticType = ""
-
-    private lateinit var viewModel: CameraViewModel
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val image = result.data?.data as Uri
+            image.let {
+                getImage = uriToFile(it, this)
+                currentImagePath = getImage.absolutePath
+                binding.imageView.setImageURI(it)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,9 +89,15 @@ class CameraActivity : AppCompatActivity() {
             }
         }
 
+        binding.btnGallery.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            launcherGallery.launch(intent)
+        }
+
         binding.btnUpload.setOnClickListener {
             if (getImage != null) {
-                val image = getImage as File
+                val image = getImage
                 val requestImage = image.asRequestBody("image/jpeg".toMediaType())
                 val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
                     "file",
@@ -93,13 +115,14 @@ class CameraActivity : AppCompatActivity() {
                                 btnUpload.isEnabled = false
                             }
                             plasticType = it.plasticType
+                            Toast.makeText(this, getString(R.string.success_to_detect), Toast.LENGTH_SHORT).show()
                         }
                         is Indicator.Loading -> {
                             binding.progressBarCamera.visibility = View.VISIBLE
                         }
                         is Indicator.Error -> {
                             binding.progressBarCamera.visibility = View.GONE
-                            Toast.makeText(this, "Terjadi kesalahan saat memproses pendeteksian : ($it)", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, getString(R.string.failed_to_detect, it), Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
@@ -117,6 +140,24 @@ class CameraActivity : AppCompatActivity() {
     private fun createTempFile(context: Context): File {
         val storageDirectory = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile("plastic $timeStamp", ".jpg", storageDirectory)
+    }
+
+    private fun uriToFile(uri: Uri, context: Context): File {
+        val contentResolver = context.contentResolver
+        val file = createTempFile(context)
+
+        val inputStream: InputStream = contentResolver.openInputStream(uri) as InputStream
+        val outputStream: OutputStream = FileOutputStream(file)
+
+        val bufferSize = ByteArray(1024)
+        var temp: Int
+        while (inputStream.read(bufferSize).also { temp = it } > 0) {
+            outputStream.write(bufferSize, 0, temp)
+        }
+        inputStream.close()
+        outputStream.close()
+
+        return file
     }
 
     companion object {
